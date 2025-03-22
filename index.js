25074ada5f87dded7dca138cac09659a1e3e0018
Olecsii3050 <vyriy3050@gmail.com>
2025-03-22 20:02:50 +0000

    Deploying to gh-pages from @ Olecsii3050/goit-js-hw-11@14624045037ad16599e2e0e9e46d091a9bd25553 🚀

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-/* empty css                      */(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const o of r.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&i(o)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();
+import{S as p}from"./assets/vendor-BdOIR_hq.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const a of t.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&n(a)}).observe(document,{childList:!0,subtree:!0});function s(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function n(e){if(e.ep)return;e.ep=!0;const t=s(e);fetch(e.href,t)}})();const u="49366841-2722787d336af87d57ebdc2bd",f="https://pixabay.com/api/",m=async r=>(await axios.get(f,{params:{key:u,q:r,image_type:"photo",orientation:"horizontal",safesearch:!0}})).data.hits;let i;const y=r=>{const o=document.querySelector(".gallery");if(o.innerHTML="",r.length===0){L("Sorry, there are no images matching your search query. Please try again!");return}const s=r.map(({webformatURL:n,largeImageURL:e,tags:t,likes:a,views:c,comments:l,downloads:d})=>`
+        <li>
+            <a href="${e}" class="gallery__item">
+                <img src="${n}" alt="${t}" loading="lazy" />
+            </a>
+            <div class="info">
+                <p>Likes: <span>${a}</span></p>
+                <p>Views: <span>${c}</span></p>
+                <p>Comments: <span>${l}</span></p>
+                <p>Downloads: <span>${d}</span></p>
+            </div>
+        </li>
+    `).join("");o.insertAdjacentHTML("beforeend",s),i?i.refresh():i=new p(".gallery a",{captionsData:"alt",captionDelay:250})},g=()=>{document.querySelector(".loader").style.display="block"},h=()=>{document.querySelector(".loader").style.display="none"},L=r=>{iziToast.error({title:"Error",message:r})},b=document.querySelector(".form");b.addEventListener("submit",async r=>{r.preventDefault();const o=r.target.elements["search-text"].value.trim();if(o){g();try{const s=await m(o);y(s)}catch(s){console.error("Error fetching images:",s),showMessage("Failed to fetch images. Please try again later.")}finally{h()}}});
 //# sourceMappingURL=index.js.map
